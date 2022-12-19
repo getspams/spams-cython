3cdbc0306750fd271b81f491206524a90891ca9d
nightwnvol <clorindo.notte@univr.it>
2022-12-19 14:02:23 +0100

    build: add some metadata

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+# NOTE this is for compatibility with legacy builds or versions of tools that don’t support certain packaging standards (e.g. PEP 517 or PEP 660)
+setup(url='https://github.com/getspams/spams-cython') # NOTE this is to show the 'Home-page' field when run 'pip show dmri-amico' (open bug https://github.com/pypa/pip/issues/11221)
