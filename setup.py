8b735717727a7a559332e38d87e6d41ad8116872
nightwnvol <clorindo.notte@univr.it>
2023-03-08 15:23:46 +0100

    build: delete setup.py

--- setup.py
+++
@@ -1,3 +0,0 @@
-# NOTE this is for compatibility with legacy builds or versions of tools that don’t support certain packaging standards (e.g. PEP 517 or PEP 660)
-from setuptools import setup
-setup()
